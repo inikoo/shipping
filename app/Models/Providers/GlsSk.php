5da8ee95b6d199e7a3db6db9d9e8ecdc4714ec68
Raul Perusquia <raul@inikoo.com>
2020-09-12 01:28:39 +0800

    GLS SK

--- app/Models/Providers/GlsSk.php
+++ app/Models/Providers/GlsSk.php
@@ -12,6 +12,9 @@ use App\Models\PdfLabel;
 use App\Models\ShipperAccount;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use SoapClient;
+use SoapFault;
+use stdClass;
 
 /**
  * Class GlsSk
@@ -25,8 +28,7 @@ class GlsSk extends Shipper_Provider {
 
     protected $table = 'gls_sk_shipper_providers';
 
-    protected $api_url = 'https://api.test.mygls.hu/ParcelService.svc/json/';
-
+    protected $api_url = "https://api.mygls.sk/ParcelService.svc?singleWsdl";
 
     protected $credentials_rules = [
         'username'      => [
@@ -44,105 +46,114 @@ class GlsSk extends Shipper_Provider {
     public function createLabel(Request $request, ShipperAccount $shipperAccount) {
 
 
-        $params = [
+        $printLabelsRequest = array(
             'Username'   => $shipperAccount->credentials['username'],
-            'Password'   => array_values(unpack('C*', hex2bin($shipperAccount->credentials['password']))),
-            'ParcelList' => [
-                $this->get_shipment_parameters($request, $shipperAccount)
-            ]
-        ];
+            'Password'   => hex2bin($shipperAccount->credentials['password']),
+            'ParcelList' => $this->get_shipment_parameters($request, $shipperAccount)
+        );
 
-        print_r($this->get_shipment_parameters($request, $shipperAccount));
+        $request = array("printLabelsRequest" => $printLabelsRequest);
 
-        $apiResponse = $this->call_api($this->api_url.'PrintLabels', $params);
+        $soapOptions = array(
+            'soap_version'   => SOAP_1_1,
+            'stream_context' => stream_context_create(array('ssl' => array('cafile' => '../assets/ca_cert.pem')))
+        );
 
 
-        $result = [];
-        if (count($apiResponse['data']['PrintLabelsErrorList']) > 0) {
-            $result['errors'] = $apiResponse['data']['PrintLabelsErrorList'];
-        } elseif (count($apiResponse['data']['Labels']) > 0) {
+        try {
 
-            $pdfData     = implode(array_map('chr', $apiResponse['data']['Labels']));
-            $pdfChecksum = md5($pdfData);
-            $pdfLabel    = new PdfLabel(
-                [
-                    'checksum' => $pdfChecksum,
-                    'data'     => base64_encode($pdfData)
-                ]
-            );
-            $shipperAccount->pdf_labels()->save($pdfLabel);
+            $client = new SoapClient($this->api_url, $soapOptions);
 
-            $result['tracking_number'] = $apiResponse['data']['PrintLabelsInfoList'][0]['ParcelNumber'];
-            $result['shipment_id']     = $apiResponse['data']['PrintLabelsInfoList'][0]['ParcelId'];
-            $result['label_link']      = env('APP_URL').'/labels/'.$pdfChecksum;
+            $apiResponse = $client->PrintLabels($request)->PrintLabelsResult;
 
+            $result = [];
+            if (count((array)$apiResponse->PrintLabelsErrorList)) {
+                $result['errors'] = [$apiResponse->PrintLabelsErrorList];
+            } elseif ($apiResponse->Labels != "") {
+
+                $pdfData     = $apiResponse->Labels;
+                $pdfChecksum = md5($pdfData);
+                $pdfLabel    = new PdfLabel(
+                    [
+                        'checksum' => $pdfChecksum,
+                        'data'     => base64_encode($pdfData)
+                    ]
+                );
+                $shipperAccount->pdf_labels()->save($pdfLabel);
+
+                $result['tracking_number'] = $apiResponse->PrintLabelsInfoList->PrintLabelsInfo->ParcelNumber;
+                $result['shipment_id']     = $apiResponse->PrintLabelsInfoList->PrintLabelsInfo->ParcelId;
+                $result['label_link']      = env('APP_URL').'/labels/'.$pdfChecksum;
+
+            }
+
+
+
+        } catch (SoapFault $e) {
+            $result['errors'] =['Soap API connection error'];
         }
-
-
         return $result;
-
 
     }
 
 
     function prepareShipment($shipperAccount, $request, $pickUp, $shipTo, $parcels, $cash_on_delivery) {
 
-        // Still in testing
 
         $tenant = $shipperAccount->tenant;
 
         $shipTo = array_filter($shipTo);
 
-        $pickupDate = "/Date(".(strtotime(Arr::get($pickUp, 'date')." 23:59:59") * 1000).")/";
-
-        return [
-            'ClientNumber'    => $shipperAccount->credentials['client_number'],
-            'ClientReference' => $request->get('reference'),
-            'CODAmount'       => 0,
-            'CODReference'    => 'COD REFERENCE',
-            'Content'         => 'CONTENT',
-            'Count'           => 1,
-            'DeliveryAddress' => array(
-                'City'            => Arr::get($shipTo, 'locality'),
-                'ContactEmail'    => Arr::get($shipTo, 'email'),
-                'ContactName'     => Arr::get($shipTo, 'contact'),
-                //  'ContactPhone'   => Arr::get($shipTo, 'phone',Arr::get($tenant->data,'phone')),
-               'ContactPhone'    => '+36701234567',
-                'CountryIsoCode'  => Arr::get($shipTo, 'country_code'),
-                'HouseNumber'     => '',
-                'Name'            => Arr::get($shipTo, 'organization', Arr::get($shipTo, 'contact')),
-                'Street'          => trim(Arr::get($shipTo, 'address_line_1').' '.Arr::get($shipTo, 'address_line_2')),
-                'ZipCode'         => trim(Arr::get($shipTo, 'sorting_code').' '.Arr::get($shipTo, 'postal_code')),
-                'HouseNumberInfo' => '',
-            ),
-            'PickupAddress'   => array(
-                'City'            => Arr::get($tenant->data['address'], 'locality'),
-                'ContactEmail'    => Arr::get($tenant->data, 'email'),
-                'ContactName'     => Arr::get($tenant->data, 'contact'),
-                'ContactPhone'    => Arr::get($tenant->data, 'phone'),
 
 
-                // 'CountryIsoCode'            => Arr::get($tenant->data['address'], 'country_code'),
-                'CountryIsoCode'  => 'HU',
-                'HouseNumber'     => '',
-                'Name'            => 'Pickup Address',
-                'Street'          => trim(Arr::get($tenant->data['address'], 'address_line_1').' '.Arr::get($tenant->data['address'], 'address_line_2')),
-                //'ZipCode'         => trim(Arr::get($tenant->data['address'], 'sorting_code').' '.Arr::get($tenant->data['address'], 'postal_code')),
-                'ZipCode'         => '2351',
+        $tenant_postal_code=trim(Arr::get($tenant->data['address'], 'sorting_code').' '.Arr::get($tenant->data['address'], 'postal_code'));
+        if(Arr::get($tenant->data['address'], 'country_code')=='SK'){
+            $tenant_postal_code=preg_replace('/SK-/i','',$tenant_postal_code);
+        }
 
-                'HouseNumberInfo' => '',
-            ),
-            'PickupDate'      => $pickupDate,
-            'ServiceList'     => array(
-                0 => array(
-                    'Code'         => 'PSD',
-                    'PSDParameter' => array(
-                        'StringValue' => '2351-CSOMAGPONT',
-                    ),
-                ),
-            ),
+        $parcels                          = [];
+        $parcel                           = new StdClass();
+        $parcel->ClientNumber             = $shipperAccount->credentials['client_number'];
+        $parcel->ClientReference          = "TEST".$request->get('reference');
+        //$parcel->CODAmount                = 0;
+        //$parcel->CODReference             = "COD REFERENCE";
+        //$parcel->Content                  = "CONTENT";
+        $parcel->Count                    = 1;
+        $deliveryAddress                  = new StdClass();
+        $deliveryAddress->ContactEmail    = Arr::get($shipTo, 'email');
+        $deliveryAddress->ContactName     = Arr::get($shipTo, 'contact');
+        $deliveryAddress->ContactPhone    = Arr::get($shipTo, 'phone');
+        $deliveryAddress->Name            = Arr::get($shipTo, 'organization' ,Arr::get($shipTo, 'contact'  ) );
+        $deliveryAddress->Street          = trim(Arr::get($shipTo, 'address_line_1').' '.Arr::get($shipTo, 'address_line_2'));
+        $deliveryAddress->City            = Arr::get($shipTo, 'locality');
+        $deliveryAddress->ZipCode         = trim(Arr::get($shipTo, 'sorting_code').' '.Arr::get($shipTo, 'postal_code'));
+        $deliveryAddress->CountryIsoCode  = Arr::get($shipTo, 'country_code');
+        $parcel->DeliveryAddress          = $deliveryAddress;
+        $pickupAddress                    = new StdClass();
+        $pickupAddress->ContactName       = Arr::get($tenant->data, 'contact');
+        $pickupAddress->ContactPhone      = Arr::get($tenant->data, 'phone');
+        $pickupAddress->ContactEmail      = Arr::get($tenant->data, 'email');
+        $pickupAddress->Name              = Arr::get($tenant->data, 'organization');
+        $pickupAddress->Street            = trim(Arr::get($tenant->data['address'], 'address_line_1').' '.Arr::get($tenant->data['address'], 'address_line_2'));
+        $pickupAddress->City              = Arr::get($tenant->data['address'], 'locality');
+        $pickupAddress->ZipCode           = $tenant_postal_code;
+        $pickupAddress->CountryIsoCode    = Arr::get($tenant->data['address'], 'country_code');
+        $parcel->PickupAddress            = $pickupAddress;
+        $parcel->PickupDate               = gmdate('Y-m-d');
 
-        ];
+        //$service1                         = new StdClass();
+        //$service1->Code                   = "FDS";
+        //$parameter1                       = new StdClass();
+        //$service1->FDSParameter           = $parameter1;
+        $services                         = [];
+       // $services[]                       = $service1;
+        $parcel->ServiceList              = $services;
+
+        $parcels[] = $parcel;
+
+
+        return $parcels;
+
 
     }
 
