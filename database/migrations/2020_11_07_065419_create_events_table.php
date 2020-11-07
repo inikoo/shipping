ca3a6a374083f801ffb3458246ab0cc1b279be60
Raul Perusquia <raul@inikoo.com>
2020-11-07 17:57:26 +0800

    events table

---
+++ database/migrations/2020_11_07_065419_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->dateTimeTz('date', 0);
+            $table->foreignId('shipment_id')->constrained();
+            $table->string('box')->nullable()->index();
+            $table->string('type')->nullable()->index();
+            $table->string('code')->nullable()->index();
+            $table->unsignedSmallInteger('state_index')->nullable()->index();
+
+            $table->jsonb('data');
+            $table->timestampsTz();
+            $table->unique(['date','shipment_id','box','code']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
